0be3585c80cec7c5f7471a39a68cd293ee384d98
TaTa6666 <107375887+TaTa6666@users.noreply.github.com>
2023-12-08 14:21:03 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,8 @@ import csv
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
 @app.route('/')
-def hello_world():  # put application's code here
+def page():
     return render_template('index.html')
 
 @app.route('/api/mydata', methods=['GET'])
@@ -23,12 +18,11 @@ def city(name):
         reader = csv.DictReader(f)
         for row in reader:
             if row['city'] == name:
-                return render_template('city.html', **row)
+                return render_template('us-cities.html', **row)
         return "City not found", 404
 
 def get_data():
     data = []
-    # 替换为你的CSV文件路径
     with open('static/amazon-reviews.csv', 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
